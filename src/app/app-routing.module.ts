e6ee05552b4b0a4d6b3bb506cd8b878b87e4f33b
Pedrol359 <84610112+Pedrol359@users.noreply.github.com>
2022-04-28 01:24:20 -0500

    asd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,34 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 
+import { LoginAdminAsociacionComponent } from './components/login-admin-asociacion/login-admin-asociacion.component';
+import { RegistrarAdminAsociacionComponent } from './components/registrar-admin-asociacion/registrar-admin-asociacion.component';
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginAdminAsociacionComponent
+  },
+  {
+    path: 'login-admin-asociacion',
+    component: LoginAdminAsociacionComponent
+  },
+  {
+    path: 'registrar-admin-asociacion',
+    component: RegistrarAdminAsociacionComponent
+  },
+  {
+    path:'**', // Este siempre debe ir al ultimo, esto sirve para mandar a una interfaz por defecto cuando la url esta mal escrita o no existe
+    redirectTo: '', pathMatch:'full'
+  }
+
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
