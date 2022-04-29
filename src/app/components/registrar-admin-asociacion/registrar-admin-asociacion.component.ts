b0b3bafe1b9a9892d9ac4f75289db3e2ec12a728
Pedrol359 <84610112+Pedrol359@users.noreply.github.com>
2022-04-29 08:39:21 -0500

    Registrar administrador al 80%

--- src/app/components/registrar-admin-asociacion/registrar-admin-asociacion.component.ts
+++ src/app/components/registrar-admin-asociacion/registrar-admin-asociacion.component.ts
@@ -8,8 +8,43 @@ import { Component, OnInit } from '@angular/core';
 export class RegistrarAdminAsociacionComponent implements OnInit {
 
   constructor() { }
-
+  //Variables
+  tipo = "password"
+  tipo1 = "password"
+  verPassword=false;
+  ocu1 ="mostrar"
+  ocu2 = "ocultar"
+  ocu3 ="mostrar"
+  ocu4 = "ocultar"
   ngOnInit(): void {
+  }
+
+  verPassword_clic(){
+    this.verPassword=!this.verPassword
+    if(this.verPassword){
+      this.tipo = "text"
+      this.ocu2 = "mostrar"
+      this.ocu1 ="ocultar"
+      
+    }else{
+      this.tipo = "password"
+      this.ocu1 ="mostrar"
+      this.ocu2 = "ocultar"
+    }
+  }
+
+  verPassword_clic1(){
+    this.verPassword=!this.verPassword
+    if(this.verPassword){
+      this.tipo1 = "text"
+      this.ocu4 = "mostrar"
+      this.ocu3 ="ocultar"
+      
+    }else{
+      this.tipo1 = "password"
+      this.ocu3 ="mostrar"
+      this.ocu4 = "ocultar"
+    }
   }
 
 }
