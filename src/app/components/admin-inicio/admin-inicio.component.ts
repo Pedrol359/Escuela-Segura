b52d93b431dbed3b0b9de2b77b0e84653a7bd8b6
Pedrol359 <84610112+Pedrol359@users.noreply.github.com>
2022-05-13 05:32:18 -0500

    panel admin 29%

--- src/app/components/admin-inicio/admin-inicio.component.ts
+++ src/app/components/admin-inicio/admin-inicio.component.ts
@@ -13,7 +13,34 @@ export class AdminInicioComponent implements OnInit {
   longitud = -104.92007528951542;
   link_video = 'https://www.youtube.com/watch?v=fqcbNFHsLFs&ab_channel=Asociaci%C3%B3nProgresoparaM%C3%A9xico'
   video_code = ''
-  estado =[false,false,false,false,false,false,false,false,false,false]
+  // estado =[false,false,false,false,false,false,false,false,false,false]
+  estado =[true,false,true,false,false]
+  nombres=[
+    "Instituto Tecnológico de Tepic",
+    "Universidad Autonoma de Nayarit",
+    "Universidad Tecnológica de Tepic",
+    "Universidad Politécnica del Estado de Nayarit",
+    "Universidad Vizcaya de las Américas",
+    "CONALEP Tepic II 310",
+    "CONALEP Tepic 169",
+    "Colegio de Bachilleres del Estado de Nayarit",
+    "Preparatoria del Valle",
+    "Colegio Herbart"
+  ]
+  claves=["123456678","123456778","23456789","FT678902","GT567892","BFGHJ56789","2345678HJW","DUIKAN15678","DFYHJ567890","DFGHJK56789"]
+  niveles=["Superior","Superior","Superior","Superior","Superior","Medio","Medio","Medio","Medio","Medio"]
+  direcciones=[
+    "Av. Tecnológico 2595, Lagos del Country, 63 ...",
+    "Boulevard Tepic-Xalisco #325 C.P. 63155, Ciu...",
+    "Carretera México 200, Km 9 63786, Col, 24 d...",
+    "Calle Dr Ignacio Cuesta Barrera S/N Carreter...",
+    "Calle Miñon Pte 7, Centro, 63000 Tepic, Nay.",
+    "Alejandrina esquina con, Topacio, 63173 Tepi...",
+    "Av. de las Torres 3, Col, Lomas de la Cruz, 63...",
+    "Av Del Ejército 107, Fray Junípero Serra, 6316...",
+    "Av. de la Cultura No. 30, Cd del Valle, 63199 ...",
+    "Tokio 8, Cd del Valle, 63157 Tepic, Nay."
+  ]
   constructor() { }
 
   ngOnInit(): void {
