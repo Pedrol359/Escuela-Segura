55ce64972fc622341475a25cf9b40787d3771e2c
Axel Jacobo <juaxjacoboco@ittepic.edu.mx>
2022-05-13 05:20:31 -0600

    Panel_editor 60%

--- src/app/components/panel-editor/panel-editor.component.ts
+++ src/app/components/panel-editor/panel-editor.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelEditorComponent implements OnInit {
 
+  articulos:string[]=["../../../assets/icons/Panel-editor/destacado.svg","../../../assets/icons/Panel-editor/destacado2.svg",
+  "../../../assets/icons/Panel-editor/destacado3.svg","../../../assets/icons/Panel-editor/destacado.svg",
+  "../../../assets/icons/Panel-editor/destacado2.svg","../../../assets/icons/Panel-editor/destacado3.svg",
+  "../../../assets/icons/Panel-editor/destacado.svg","../../../assets/icons/Panel-editor/destacado2.svg",
+  "../../../assets/icons/Panel-editor/destacado3.svg","../../../assets/icons/Panel-editor/destacado.svg",
+  "../../../assets/icons/Panel-editor/destacado2.svg","../../../assets/icons/Panel-editor/destacado3.svg"]
+  
+  selected_article:string = "../../../assets/icons/Panel-editor/selected-article.svg";
   constructor() { }
 
   ngOnInit(): void {
