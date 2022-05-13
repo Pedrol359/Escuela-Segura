ea62f13f8a95df20594ed1142740f294b6df62eb
Pedrol359 <84610112+Pedrol359@users.noreply.github.com>
2022-05-13 04:22:55 -0500

    panel de admin al 25%

--- src/app/components/admin-inicio/admin-inicio.component.ts
+++ src/app/components/admin-inicio/admin-inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as Mapboxgl from 'mapbox-gl';
+import { environment } from '../../../environments/environment.prod';
 
 @Component({
   selector: 'app-admin-inicio',
@@ -7,9 +9,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminInicioComponent implements OnInit {
 
+  latitud = 21.507029616565315;
+  longitud = -104.92007528951542;
+  link_video = 'https://www.youtube.com/watch?v=fqcbNFHsLFs&ab_channel=Asociaci%C3%B3nProgresoparaM%C3%A9xico'
+  video_code = ''
+  estado =[false,false,false,false,false,false,false,false,false,false]
   constructor() { }
 
   ngOnInit(): void {
+    this.getLocation();
+    this.getVideoCode();
+    console.log(this.estado[0]);
+    
   }
 
+  getLocation(){
+    (Mapboxgl as any).accessToken = environment.mapboxKey;
+    var mapa = new Mapboxgl.Map({
+      container: 'map-box', // container CSS
+      style: 'mapbox://styles/mapbox/streets-v11', //mapbox://styles/mapbox/streets-v11 (LIGHT)
+      center: [this.longitud, this.latitud], // LONGITUD,LATITUD
+      zoom: 11 // Zoom inicial
+    });
+
+    var marker = new Mapboxgl.Marker({
+      draggable: false
+    }).setLngLat([this.longitud, this.latitud]).addTo(mapa);
+
+    // Add zoom and rotation controls to the map.
+    mapa.addControl(new Mapboxgl.NavigationControl());
+  }
+
+  getVideoCode() {
+    var code = this.link_video.split("?v=",2);
+    this.video_code = code[1];
+    console.log(this.video_code);
+    code = this.video_code.split("&",2);
+    console.log(this.video_code);
+    this.video_code = 'https://www.youtube.com/embed/'+code[0];
+    console.log(this.video_code);
+  }
+
+  estados(){
+    this.estado[0] =! this.estado[0]
+    console.log(this.estado[0]);
+    
+    
+  }
 }
