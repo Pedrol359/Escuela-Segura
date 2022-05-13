91849f6e39710c473c873c3b90f72d17e2a849ff
Axel Jacobo <juaxjacoboco@ittepic.edu.mx>
2022-05-13 08:20:30 -0600

    Panel de editor
    First view, changes needed

--- src/app/components/panel-editor/panel-editor.component.ts
+++ src/app/components/panel-editor/panel-editor.component.ts
@@ -15,9 +15,19 @@ export class PanelEditorComponent implements OnInit {
   "../../../assets/icons/Panel-editor/destacado2.svg","../../../assets/icons/Panel-editor/destacado3.svg"]
   
   selected_article:string = "../../../assets/icons/Panel-editor/selected-article.svg";
+  titulo_input:string="0/35";
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getCharacters(limite:number,id_element:string) {
+    var titulo:string = "0/35";
+    var input =<HTMLInputElement> document.getElementById(id_element);
+    var indicador:string = "";
+    indicador = input.value.length+'\\'+limite;
+    this.titulo_input = indicador;
   }
 
 }
