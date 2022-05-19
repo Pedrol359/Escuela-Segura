79eb7841ec9baeef483a224be83984fea54354d8
Axel Jacobo <juaxjacoboco@ittepic.edu.mx>
2022-05-19 16:08:49 -0600

    Create carousel.const.nosotros.ts

---
+++ src/app/components/nosotros/carousel.const.nosotros.ts
@@ -0,0 +1,44 @@
+import {ICarouselItem} from '../carousel/Icarousel-item.metadata'
+
+export const CAROUSEL_DATA_TIPOS: ICarouselItem[] = [
+    {
+        id: 1,
+        title: {
+            first: 'Violencia emocional',
+            second: 'Principal'
+        },
+        subtitle: 'Esto es una gran descripción',
+        link: '/',
+        image: 'assets/carousel/tipos-violencia/1.svg'
+    },
+    {
+        id: 2,
+        title: {
+            first: 'Violencia patrimonial',
+            second: 'Segundo'
+        },
+        subtitle: 'Esto es una gran descripción',
+        link: '/',
+        image: 'assets/carousel/tipos-violencia/2.svg'
+    },
+    {
+        id: 3,
+        title: {
+            first: 'Violencia física',
+            second: 'Tercero'
+        },
+        subtitle: 'Esto es una gran descripción',
+        link: '/',
+        image: 'assets/carousel/tipos-violencia/3.svg'
+    },
+    {
+        id: 4,
+        title: {
+            first: 'Violencia sexual',
+            second: 'Cuarto'
+        },
+        subtitle: 'Esto es una gran descripción',
+        link: '/',
+        image: 'assets/carousel/tipos-violencia/4.svg'
+    }
+];
