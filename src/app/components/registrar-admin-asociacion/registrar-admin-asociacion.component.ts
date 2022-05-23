e29974b8157bac204db83f09b2c46ef3f8425724
Axel Jacobo <juaxjacoboco@ittepic.edu.mx>
2022-05-23 17:48:48 -0600

    Login terminado

--- src/app/components/registrar-admin-asociacion/registrar-admin-asociacion.component.ts
+++ src/app/components/registrar-admin-asociacion/registrar-admin-asociacion.component.ts
@@ -31,7 +31,6 @@ export class RegistrarAdminAsociacionComponent implements OnInit {
   //Encriptación de contraseña
   passwordEncryp = ""
   passwordDesencryp = ""
-  passwordFunctionEncrypt=""
   //Objeto registro
   registro= {}
 
