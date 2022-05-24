eb12ad2bd0bdbc1ebe30df7fdf5447f7a3d3b313
Bradley Addiel González Flores <bradgonzalezfl@ittepic.edu.mx>
2022-05-24 08:19:57 -0600

    Revert "Seleccionar contenido destacado basic"
    This reverts commit c5644155d9fc1f453eda4527c64cc96fca7b7e17.

--- src/app/services/Articulos.service.ts
+++ src/app/services/Articulos.service.ts
@@ -13,7 +13,6 @@ export class ArticuloService {
     //Objeto Articulo
     articulo = {
         id: "",
-        destacado: "",
         titulo: "",
         autor: "",
         descripcion: "",
@@ -30,7 +29,7 @@ export class ArticuloService {
     actualizarArticulo(articulo: any): Promise<any> {
         return this.firestore.collection('Articulos').doc(articulo.id).update(articulo)
     }
-    eliminarArticulo(idArticulo: string) {
+    eliminarArticulo(idArticulo:string){
         return this.firestore.collection('Articulos').doc(idArticulo).delete()
     }
 
