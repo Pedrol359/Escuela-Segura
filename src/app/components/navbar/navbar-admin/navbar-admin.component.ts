a7e845f3d9c450b45e0515a07d35dd76a4d8d2df
Axel Jacobo <juaxjacoboco@ittepic.edu.mx>
2022-05-24 20:26:28 -0600

    Nav-bars responsivas

--- src/app/components/navbar/navbar-admin/navbar-admin.component.ts
+++ src/app/components/navbar/navbar-admin/navbar-admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar-admin',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarAdminComponent implements OnInit {
 
-  constructor() { }
+  screenWidth:any
+  display_full = 'flex';
+  display_responsive = 'none';
+
+  constructor() {
+      this.getScreenSize();
+   }
+
 
   ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
   }
+
+  @HostListener('window:resize', ['$event'])
+    getScreenSize() {
+      this.screenWidth = window.innerWidth;
+      console.log(this.screenWidth);
+      if(this.screenWidth < 1330) this.dropDownOn();
+      else this.fullOn();
+  }
+
+  dropDownOn(){
+    this.display_responsive = "flex"
+    this.display_full = "none"
+  }
+
+  fullOn(){
+    this.display_responsive = "none"
+    this.display_full = "flex"
+  }
+
 
 }
--- src/app/components/navbar/navbar-principal/navbar-principal.component.ts
+++ src/app/components/navbar/navbar-principal/navbar-principal.component.ts
@@ -8,16 +8,33 @@ import { Component, OnInit, HostListener } from '@angular/core';
 export class NavbarPrincipalComponent implements OnInit {
 
   screenWidth:any
+  display_full = 'flex';
+  display_responsive = 'none';
 
-  constructor() { }
+  constructor() {
+      this.getScreenSize();
+   }
 
   ngOnInit(): void {
     this.screenWidth = window.innerWidth;
   }
 
-  @HostListener('window:resize')
-onResize() {
-  this.screenWidth = window.innerWidth
-}
+  @HostListener('window:resize', ['$event'])
+    getScreenSize() {
+      this.screenWidth = window.innerWidth;
+      console.log(this.screenWidth);
+      if(this.screenWidth < 1330) this.dropDownOn();
+      else this.fullOn();
+  }
+
+  dropDownOn(){
+    this.display_responsive = "flex"
+    this.display_full = "none"
+  }
+
+  fullOn(){
+    this.display_responsive = "none"
+    this.display_full = "flex"
+  }
 
 }
