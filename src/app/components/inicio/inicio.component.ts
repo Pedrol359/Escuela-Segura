de9ef575036e7aecd56461986277862274a66d08
Axel Jacobo <juaxjacoboco@ittepic.edu.mx>
2022-05-25 15:57:00 -0600

    Inicio consulta artículos destacados

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { ArticuloService } from 'src/app/services/Articulos.service';
 
 
 @Component({
@@ -9,7 +10,15 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class InicioComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  // Contenido destacado
+  articulos: any[] = []
+  articulos_destacados: any[] = []
+  eliminados: string[] = []
+  destacado1 = '';
+  destacado2 = '';
+  destacado3 = '';
+
+  constructor(private router: Router,private _articulo: ArticuloService) { }
 
   ngOnInit(): void {
     this.router.events.subscribe((evt) => {
@@ -18,6 +27,50 @@ export class InicioComponent implements OnInit {
       }
       window.scrollTo(0, 0)
     });
+    this.obtenerArticulos();
+  }
+
+  obtenerArticulos() {
+    let subs = this._articulo.obtenerArticulos().subscribe(data => {
+        this.articulos = [];
+        this.articulos_destacados = [];
+        data.forEach((element: any) => {
+            this.articulos.push({
+                ...element.payload.doc.data(),
+                id: element.payload.doc.id
+            })
+        })
+        subs.unsubscribe
+        this.obtenerArticulosDestacados();
+        // console.log(this.articulos_destacados);
+    });
+  }
+
+  obtenerArticulosDestacados() {
+    // Con este método filtramos los artículos destacados de la
+    // lista de artículos obtenidos
+    for (var a in this.articulos) {
+        if (this.articulos[a].destacado == '0') {
+            this.destacado1 = a;
+            this.articulos_destacados[0] = this.articulos[a];
+        }
+
+        if (this.articulos[a].destacado == '1') {
+            this.destacado2 = a;
+            this.articulos_destacados[1] = this.articulos[a];
+        }
+
+        if (this.articulos[a].destacado == '2') {
+            this.destacado3 = a;
+            this.articulos_destacados[2] = this.articulos[a];
+        }
+    }
+    console.log('[Artículo Destacados]');
+    console.log(this.articulos_destacados);
+  }
+
+  formatearUrl(url: string) {
+    return 'center/cover url(' + url + ')';
   }
 
 }
